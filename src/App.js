bd9ded91f68f4434e1f5104cc5a4a5920ff5d7c2
Lautaro Montaña <joselautarom@gmail.com>
2022-09-21 20:31:08 -0300

    Creamos to do lis

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Creamos un To do list & Timer</h1>
+        <Todo/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.jsx
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+const Todo = () => {
+
+    const [todos, setTodos] = useState([])
+
+    const[todo, setTodo] = useState("")
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const newTodo = {
+            id: new Date().getTime(),
+            text: todo,
+            completed: false,
+        }
+        setTodos([...todos].concat(newTodo))
+        setTodo("")
+        console.log(setTodo)
+    }
+    const deleteTodo = (id) =>{
+        const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+        setTodos(updatedTodos)
+    }
+
+    return(
+        <div>
+            <h2>Nuestras tareas</h2>
+            <h3>Ingrese sus tareas aquí 👇</h3>
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                placeholder="Ingrese tarea aquí" 
+                onChange={(e) => setTodo (e.target.value)}></input>
+                <button type="submit">➕</button>
+            </form>
+            {
+                todos.map((todo) =>
+                <div>
+                    <div>{todo.text}<button onClick={() => deleteTodo(todo.id)}>❌</button></div>
+                </div>)}
+        </div>
+    )
+
+}
+export default Todo
